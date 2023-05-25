9eec352ab268f7c7dfee22453eeaf302e69c013a
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-05-25 10:31:29 -0300

    segunda commit

--- 1-Materiais de aula/12-Aula12/Exemplos/main.c
+++ 1-Materiais de aula/12-Aula12/Exemplos/main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <ctype.h> //Biblioteca para caracteres
+#include <ctype.h>
 #include <locale.h>
 
 int main() {
 	system("cls");
 	setlocale(LC_ALL,"");
-	float n1, n2;
-	char op;
-	printf("Calculadora\n");
-	scanf("%f %c%f",&n1,&op,&n2);
+	int i, conta=0, contr=0;
+	float n1, n2, med;
+	for(i=0;i<5;i++){
+		printf("\n\nInsira as notas do %dº aluno:",i+1);
+		scanf("%f%f",&n1,&n2);
+		if(n1 >= 0 && n1 <=10 && n2 >= 0 && n2 <= 10 ){
+			med = (n1+n2)/2;
+			printf("\nA média do %dº aluno é: %.2f",i+1,med);
+			if(med>=6){
+				conta++;
+			}else{
+				contr++;
+				}
+		}else{
+			printf("\nNota inválida, tente novamente!\n");
+			i--;
+		}
+}
+	printf("\n \nO total de alunos APROVADOS foi: %d alunos",conta);
+	printf("\nO toal de alunos REPROVADOS foi: %d alunos",contr);
 	return 0;
 }
--- 1-Materiais de aula/12-Aula12/Exercicio/main.c
+++ 1-Materiais de aula/12-Aula12/Exercicio/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <locale.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
+int main() {
+	system("cls");
+	setlocale(LC_ALL,"");
+	int i, cont=0;
+	float alt, med;
+	for(i=0;i<10;i++){
+		printf("\nUsuário: %d", i+1);
+		printf("\nInsira a sua altura: ");
+		scanf("%f",&alt);
+		if(alt > 0){
+				if(alt >= 1.60){
+				cont++;
+			}	
+		}else{
+			printf("\nAltura inválida, insira novamente! \n \n");
+			i--;
+		}
+}
+	printf("\n %d usuários são maiores que 1,60",cont);
 	return 0;
 }
