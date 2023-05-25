9322fa7fb2e415801668d71c9c0597d9f79d428d
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-05-25 11:13:40 -0300

    terceira commit

--- 1-Materiais de aula/12-Aula12/Exercicio/main.c
+++ 1-Materiais de aula/12-Aula12/Exercicio/main.c
@@ -3,12 +3,11 @@
 #include <math.h>
 #include <ctype.h>
 #include <locale.h>
-
 int main() {
 	system("cls");
-	setlocale(LC_ALL,"");
+	/*setlocale(LC_ALL,"");
 	int i, cont=0;
-	float alt, med;
+	float alt;
 	for(i=0;i<10;i++){
 		printf("\nUsuário: %d", i+1);
 		printf("\nInsira a sua altura: ");
@@ -23,5 +22,15 @@ int main() {
 		}
 }
 	printf("\n %d usuários são maiores que 1,60",cont);
+	*/
+	
+	int i, cont=0;
+	for(i=65; i>=50; i--){
+		cont++;
+		printf("Entrada: %d", cont);
+		printf("\nTemperatura em Fahrenheit: %d", i);
+		printf("\nTemperatura em Celsius: %.2f \n\n", 0.56*(i-32));
+	}
+	
 	return 0;
 }
