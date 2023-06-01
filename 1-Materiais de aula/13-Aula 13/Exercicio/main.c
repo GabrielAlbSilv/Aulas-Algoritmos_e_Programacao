4845532d8579aab8e8383556b8b82896b924e3ee
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-06-01 11:11:15 -0300

    quarta commit 01/06/23

--- 1-Materiais de aula/13-Aula 13/Exercicio/main.c
+++ 1-Materiais de aula/13-Aula 13/Exercicio/main.c
@@ -6,16 +6,29 @@
 int main() {
 	system("cls");
 	setlocale(LC_ALL,"");
-	int i=0;
-	float val, acum=0, acum2=0; //sempre iniciar a variável acumladora 
-	for (i=0; i<5;i++){
+	/*int i=0;
+	float val, acum=0, acum2=0; 
+	for (i=0; i<3;i++){
 		printf("\nPor favor, insira um valor: ");
 		scanf("%f",&val);
-		acum = acum + val;
+		acum += val;
 		if (val < 50){
-			acum2 = acum2 + val;
+			acum2 += val;
 		}
-		 //acumalador somando todos os valores inseridos
+	}
+	printf("\nSoma dos valores menores que 50: %.2f", acum2);
+	printf("\nMédia de todos os valores: %.2f", acum/i);
+	*/
+	int i=0;
+	float val, acum=0, acum2=0; 
+	while (i<3){
+		printf("\nPor favor, insira um valor: ");
+		scanf("%f",&val);
+		acum += val;
+		if (val < 50){
+			acum2 += val;
+		}
+		i++;
 	}
 	printf("\nSoma dos valores menores que 50: %.2f", acum2);
 	printf("\nMédia de todos os valores: %.2f", acum/i);
